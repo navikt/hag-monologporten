6751a0a8b8ede41bf07079c5acfe66119a9e5ae5
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-04-28 13:44:40 +0200

    Legg på litt logging for å se at kafkameldinger blir konsumert

--- src/main/kotlin/kafka/KafkaConsumer.kt
+++ src/main/kotlin/kafka/KafkaConsumer.kt
@@ -4,16 +4,19 @@ import no.nav.helsearbeidsgiver.Env
 import no.nav.helsearbeidsgiver.utils.UnleashFeatureToggles
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.slf4j.LoggerFactory
 import java.time.Duration
 
 fun startKafkaConsumer(unleashFeatureToggles: UnleashFeatureToggles) {
     val consumer = KafkaConsumer<String, String>(createKafkaConsumerConfig() as Map<String, Any>)
+    val logger = LoggerFactory.getLogger(KafkaConsumer::class.java)
     val topic = Env.Kafka.topic
     consumer.subscribe(listOf(topic))
     val running = true
     while (running) {
         val records = consumer.poll(Duration.ofMillis(1000))
         for (record in records) {
+            logger.info("Fikk melding på dialog kafka topic fra partition: ${record.partition()}")
             sikkerLogger().info("Consumed message: ${record.value()} from partition: ${record.partition()}")
             sikkerLogger().info(
                 "Unleash feature toggle for dialogopprettelse: ${unleashFeatureToggles.skalOppretteDialogVedMottattSykmelding()}",
